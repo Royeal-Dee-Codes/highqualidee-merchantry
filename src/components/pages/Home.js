ffd875cb5f683b98885dcaecedeafa48e62fa10a
highqualidee <highqualidee@gmail.com>
2024-04-24 17:29:38 -0600

    fixes home page, navbar styles

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,35 +1,7 @@
-import React, { useState, useEffect } from "react";
 import image from "../../assests/images/anders-jilden-AkUR27wtaxs-unsplash.jpg";
 import "../../styles/app.scss";
 
 export default function Home() {
-  const [product, setProduct] = useState([]);
-  console.log(product);
-  useEffect(() => {
-    fakestoreapi();
-  }, []);
-
-  const fakestoreapi = async () => {
-    const response = await fetch("https://fakestoreapi.com/products?limit=5");
-    const jsonData = await response.json();
-    setProduct(jsonData);
-  };
-  <div className="main-container">
-    {product.map((value) => {
-      return (
-        <>
-          <div className="product-container">
-            <div className="content-container">
-              <h3>{value.title}</h3>
-              <p>{value.description}</p>
-            </div>
-            <img src={value.image} alt=" "></img>
-          </div>
-        </>
-      );
-    })}
-  </div>;
-
   return (
     <div className="page-container">
       <div className="page-header">
